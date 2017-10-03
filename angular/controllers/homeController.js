733da5907abaed66ca988e3fde1ce3217ec5af17
angel cotes españa <angelcotes@gmail.com>
2017-10-02 22:58:37 -0500

    ultimos cambios

--- angular/controllers/homeController.js
+++ angular/controllers/homeController.js
@@ -2,8 +2,22 @@
 
 angular.module('vista')
   .controller('homeController', function ($scope) {
-    $scope.format = -1;
-    $scope.changeFormat = function(){
-      $scope.format = $scope.format * -1;
+    $scope.realidad = true;
+    $scope.redes = true;
+    $scope.robot = true;
+    $scope.verRealidad = function(){
+      $scope.redes = true;
+      $scope.robot = true;
+      $scope.realidad = !$scope.realidad;
     };
+    $scope.verRedes = function(){
+      $scope.realidad = true;
+      $scope.robot = true;
+      $scope.redes = !$scope.redes;
+    };
+    $scope.verRobot = function(){
+      $scope.redes = true;
+      $scope.robot = !$scope.robot;
+      $scope.realidad = true;
+    }
 });
