468339d20298358973fa1023529992981e2046fd
angel cotes españa <angelcotes@gmail.com>
2017-10-02 20:22:31 -0500

    componentes de angular

--- angular/app.js
+++ angular/app.js
@@ -1,15 +1,5 @@
 'use strict';
-angular.module('Myapp', ['ngRoute'])
-.constant('BASE_URL', 'http://localhost:8080/')
-.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.useXDomain = true;
-    $httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = "*";
-    $httpProvider.defaults.headers.common['Access-Control-Allow-Methods'] = "POST, GET, OPTIONS, DELETE";
-    $httpProvider.defaults.headers.common['Access-Control-Max-Age'] = "3600";
-    $httpProvider.defaults.headers.common['Access-Control-Allow-Headers'] = "x-requested-with";
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-])
+angular.module('vista', ['ngRoute'])
 .config(function ($routeProvider) {
   $routeProvider
     .when('/home',{
